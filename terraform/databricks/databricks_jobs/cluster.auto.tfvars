6ce5449b077adbe0c7bc69f533558d399184da0c
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2023-04-23 16:24:10 +0200

    Remove work cluster in terraform

--- terraform/databricks/databricks_jobs/cluster.auto.tfvars
+++ terraform/databricks/databricks_jobs/cluster.auto.tfvars
@@ -1,4 +1,3 @@
 all_purpose_cluster_name        = "All Purpose Cluster"
-work_cluster_name               = "Work Cluster"
 cluster_autotermination_minutes = 60
 cluster_num_workers             = 2
--- terraform/databricks/databricks_jobs/cluster.tf
+++ terraform/databricks/databricks_jobs/cluster.tf
@@ -5,11 +5,6 @@ variable "all_purpose_cluster_name" {
   default     = "all_purpose_cluster"
 }
 
-variable "work_cluster_name" {
-  description = "A name for the cluster."
-  type        = string
-  default     = "work_cluster"
-}
 
 # Create the cluster with the "smallest" amount
 # of resources allowed.
@@ -18,13 +13,6 @@ data "databricks_node_type" "all_purpose_databricks_node_type" {
   min_memory_gb = 16
   min_cores     = 4
   category      = "General Purpose"
-}
-
-data "databricks_node_type" "work_databricks_node_type" {
-  local_disk    = true
-  min_memory_gb = 16
-  min_cores     = 8
-  category      = "Compute Optimized"
 }
 
 # Common configurations
@@ -54,18 +42,6 @@ resource "databricks_cluster" "all_purpose_databricks_cluster" {
   num_workers             = var.cluster_num_workers
 }
 
-resource "databricks_cluster" "work_databricks_cluster" {
-  cluster_name            = var.work_cluster_name
-  node_type_id            = data.databricks_node_type.work_databricks_node_type.id
-  spark_version           = data.databricks_spark_version.latest_lts.id
-  autotermination_minutes = var.cluster_autotermination_minutes
-  num_workers             = var.cluster_num_workers
-}
-
 output "all_purpose_cluster_url" {
   value = databricks_cluster.all_purpose_databricks_cluster.url
-}
-
-output "work_cluster_url" {
-  value = databricks_cluster.work_databricks_cluster.url
 }
